1be218b500b4b114873a536722c24daaeca6f854
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-06-05 17:19:46 -0700

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import {
   Container,
   Row,
   Col,
-  Input
+  Input,
+  Button
 } from 'reactstrap';
 
 class Footer extends Component {
@@ -14,7 +15,49 @@ class Footer extends Component {
           <Row>
             <Col>
               <p>Get our pioneering hardware picks in your inbox with Tindie Fetch.</p>
-              <Input />
+              <Input type="email" />
+              <Button>Subscribe</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <ul>
+                <li><h4>Company</h4></li>
+                <li>About Tindie</li>
+                <li>Tindie Blog</li>
+                <li>Our Terms</li>
+              </ul>
+            </Col>
+            <Col>
+              <ul>
+                <li><h4>Discover</h4></li>
+                <li>Newest Products</li>
+                <li>Popular Products</li>
+                <li>On Sale</li>
+              </ul>
+            </Col>
+            <Col>
+              <ul>
+                <li><h4>Buying & Selling</h4></li>
+                <li>Buy on Tindie</li>
+                <li>Sell on Tindie</li>
+                <li>Tindie Guarantee</li>
+              </ul>
+            </Col>
+            <Col>
+              <ul>
+                <li><h4>Help</h4></li>
+                <li>FAQs</li>
+                <li>Contact Tindie</li>
+                <li>Community Forums</li>
+              </ul>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col >
+              &copy; 2018 Tindie, Inc. Privacy Policy
             </Col>
           </Row>
         </Container>
