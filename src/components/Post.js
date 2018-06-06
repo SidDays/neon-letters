2b993768902b03113b0b07577a438c2e997f47af
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-06-06 15:16:06 -0700

    Basic tags added

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,15 +13,14 @@ class Post extends Component {
       <article>
         <Row>
           <Col >
-            <img src="http://via.placeholder.com/100x100" alt="Post Author"></img>
+            <img src="http://via.placeholder.com/70x70" alt="Post Author"></img>
+            <span>John Doe</span>
           </Col>
-          <Col >
-            John Doe
-        </Col>
+
           <Col>
             <Badge color="light">
               Tindie
-        </Badge>
+            </Badge>
 
           </Col>
           <Col >
@@ -34,10 +33,16 @@ class Post extends Component {
             <h2>A Two-line long description of the latest hardware product</h2>
             <p>
               Lorem ipsum dolor amet green juice woke wolf, artisan williamsburg fashion axe pork belly chartreuse gluten-free selfies XOXO fam. Readymade palo santo af before they sold out kickstarter intelligentsia mustache cronut flannel tattooed leggings.</p>
-            <p>Authentic viral church-key occupy lo-fi migas master cleanse enamel pin keytar copper mug pickled tacos activated charcoal. Activated charcoal bitters flannel, man bun enamel pin marfa meditation disrupt aesthetic banjo viral kitsch.</p>
             <p>
-              Vinyl four loko lomo heirloom portland selfies retro crucifix. Farm-to-table portland poke tacos hella. Coloring book selvage put a bird on it franzen wolf actually.
-        </p>
+              Authentic viral church-key occupy lo-fi migas master cleanse enamel pin keytar copper mug pickled tacos activated charcoal. Activated charcoal bitters flannel, man bun enamel pin marfa meditation disrupt aesthetic banjo viral kitsch.</p>
+            <p>
+              Vinyl four loko lomo heirloom portland selfies retro crucifix. Farm-to-table portland poke tacos hella. Coloring book selvage put a bird on it franzen wolf actually.</p>
+            <ul class="list-inline">
+              <li class="list-inline-item"><a href="#">#firsttag</a></li>
+              <li class="list-inline-item"><a href="#">#secondtag</a></li>
+              <li class="list-inline-item"><a href="#">#thirdtag</a></li>
+            </ul>
+            <hr />
           </Col>
         </Row>
       </article>)
--- src/components/PostsOverview.js
+++ src/components/PostsOverview.js
@@ -5,34 +5,17 @@ import Post from './Post';
 
 class PostsOverview extends Component {
   
-  getPosts() {
-    return (
-      <div>
-        <article>
-        This is a post
-        </article>
-        <article>
-        This is a post
-        </article>
-        <article>
-        This is a post
-        </article>
-      </div>
-    );
-  }
-  
   render() {
     return (
       <Container>
-        PostsOverview PAGE WITH POSTS
-
-
-        { this.getPosts() }
-
+        <hr />
 
         <Post />
+        <Post />
+        <Post />
 
-      </Container>)
+      </Container>
+      );
   }
 }
 
